38bf91112546639b656c00d825111da47a6e64d5
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-06-30 18:24:53 +0400

    port changes

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use balance::{get_solana_balance, Cluster};
 
 fn main() {
     let listener = 
-        TcpListener::bind("127.0.0.1:8080").unwrap();
+        TcpListener::bind("0.0.0.0:4000").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
